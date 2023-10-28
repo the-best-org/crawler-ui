35dbf530d9994b0e71ad936e79ab06148420b5a8
Ivan Enzel <enzel@live.com>
2023-10-28 13:52:52 +0500

    tests fix

--- tests/test.sh
+++ tests/test.sh
@@ -2,22 +2,10 @@
 
 echo "--- Running tests ---"
 
-if python -m unittest discover -s tests/ 2> >(grep -i OK)
-then 
-    echo "--- Tests runned ---"
-else
-    echo "Test did not ran OK"
-    exit 1
-fi
+python -m unittest discover -s tests/ 
 
 echo "--- Generating report ---"
 
-if coverage run -m unittest discover -s tests/ 2> >(grep -i OK)
-then 
-    echo "App covered with tests"
-else
-    echo "Report did not generated"
-    exit 1
-fi
+coverage run -m unittest discover -s tests/ 
 
 coverage report --include ui/ui.py
